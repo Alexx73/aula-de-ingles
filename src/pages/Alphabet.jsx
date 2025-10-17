3ef09f06899b6ef195506eae6f1d691e639fcfc3
Alexx73 <tristanluna@yahoo.com.ar>
2025-10-17 20:02:48 -0300

    nuevo comienzo

--- src/pages/Alphabet.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Alphabet() {
-  return (
-    <div className="flex h-screen min-h-screen flex-col items-center justify-center bg-gray-50 dark:bg-gray-900">
-      Alphabet
-    </div>
-  );
-}
